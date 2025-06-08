4655cdca466e05fc3d1e2a26455558fc03dc8524
Isco16-06 <alcidlorry.nkenfacketong@etu.estia.fr>
2025-06-08 17:13:30 +0200

    résolution de problème

--- src/features.h
+++ src/features.h
@@ -4,8 +4,14 @@
 
 void helloWorld();
 void dimension (const char *source_path);
+void tenth_pixel(const char *source_path);
 void second_line(const char *source_path);
 void first_pixel(char *source_path);
 void print_pixel(const char *filename, int x, int y );
+void max_pixel (const char *source_path);
+void min_pixel (const char *source_path);
+void max_component (const char *source_path, char *choice);
+void min_component (const char *source_path, char *choice);
+void color_red(const char *source_path);
 
 #endif
